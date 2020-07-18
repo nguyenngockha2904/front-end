41e99e74342d96d312d1810f5af1d7a8e8bc0d45
unknown <khanguyen1000@gmail.com>
2020-07-19 05:02:55 +0700

    update

--- javascript/DU AN 3 - CYBERSOFT - SINH VIEN/DU AN 3 - CYBERSOFT - SINH VIEN/js/main.js
+++ javascript/DU AN 3 - CYBERSOFT - SINH VIEN/DU AN 3 - CYBERSOFT - SINH VIEN/js/main.js
@@ -1,60 +1,110 @@
-/*
- Muc tieu: Xu ly quan ly trung sinh vien
- Nguoi tao: Le Buoi
- Ngay tao : 27/03/2018
- Version: 1.0
- */
-function getMyEle(ele) {
-	return document.getElementById(ele);
+/**
+ * bài tập : Quản lý sinh viên
+ * chức năng :
+ *      1. Lấy danh sách điểm từ giao diện bỏ vào mảng điểm.
+ *      2. Tính điểm trung bình của toàn bộ sinh viên.
+ *      3. Kiểm tra có bao nhiêu sinh viên dưới 5 điểm trong danh sách
+ *      4. Tìm điểm tb cao nhất và cao nhì.
+ *      5. Sắp xếp danh sách điểm tăng dần.
+*/
+
+
+// function 1 lấy danh sách điểm từ giao diện
+const gradeList = [];
+function getGradeFromUI() {
+    let td_grade = document.getElementsByClassName('td_grade');
+    // console.log(td_grade[0].innerHTML);
+    for (let i = 0; i < td_grade.length; i++) {
+        gradeList.push(+td_grade[i].innerHTML);
+    }
 }
+getGradeFromUI();
+// console.log(gradeList);
+// function 2 Tính điểm trung bình của toàn bộ sinh viên
+function CalAverage() {
+    let sum = 0;
+    for (let i = 0; i < gradeList.length; i++) sum += gradeList[i];
 
-/*
-	Muc tieu ham: Duyet Tat ca cac row cua table va luu tru vao mot mang du lieu
-	Nguoi tao : Le Buoi
-	Ngay tao: ...
-	Version: 1.0
- */
-function TaoMangDuLieu() {
-	
-	
-	var mangDuLieu = []; // tao mang
-	var tableSv = getMyEle('tableSinhVien');
-	
-	// vong lap 
-	for(var i = 1; i < tableSv.rows.length; i++){// i = i + 1, i+=1, i+=2
-	  mangDuLieu.push(tableSv.rows[i].cells[3].innerHTML); // dua du lieu vao mang
-	}
-	return mangDuLieu;
+    return sum / gradeList.length;
 }
-
-function TimMaxDTB() {
-	 var duLieu = TaoMangDuLieu();
-	 // tim max, giai thuat tim lon nhat
-	// var max = parseFloat(duLieu[0]);
-	 var indexMax = 0;
-	 // duyet mang va tim phan tu nao co dtb lon hon max thi gan lai cho max
-	 for (var i = 1; i < duLieu.length; i++) {
-	 	var dtbThu_i = parseFloat(duLieu[i]);
-	 	if(dtbThu_i > duLieu[indexMax] ){
-	 		indexMax = i; // luu chi so ma thoi
-	 	}
-	 }
-
-	 return indexMax; 
+let average = CalAverage();
+console.log(average);
+console.log(Math.floor(average)); //làm tròn xuống
+console.log(Math.ceil(average)); //làm tròn lên
+console.log(Math.round(average));
+console.log(average.toFixed(2));
+// đếm sv dưới 5 điểm
+function countWeakStudent() {
+    let count = 0;
+    for (let i = 0; i < gradeList.length; i++) {
+        if (gradeList[i] < 5) {
+            count++;
+        }
+    }
+    return count;
 }
+console.log("Weak student :" + countWeakStudent() + " person");
 
-function XuatSVCoDTBLonNhat() {
-	 // cho xuat hien div hien thi
-	 var hienThi = getMyEle('hienthi');
-	 hienThi.style.display = "block";
-
-	 var max = TimMaxDTB(); // chi so SV co dtb lon nhat trong mang
-
-	 var tableSv = getMyEle('tableSinhVien');
-	 var tenSv = tableSv.rows[max+1].cells[2].innerHTML; // Ten sinh vien o cell thu 2
-	 var dtb = tableSv.rows[max+1].cells[3].innerHTML; // DTB sinh vien o cell thu 2
-
-	 var spanHienThi = getMyEle('svGioiNhat');
-	 spanHienThi.innerHTML = "Ten: " +tenSv + "<br>" + " DTB la : " + dtb;
-
+// Tìm điểm tb cao nhất và cao nhì.
+function findMaxAndSecond() {
+    let max = gradeList[0];
+    let second = 0;
+    for (let i = 1; i < gradeList.length; i++) {
+        if (gradeList[i] > max) { //nếu nó lớn hơn max thì max hiện tại gán cho second và gáng nó cho max
+            second = max;
+            max = gradeList[i];
+        } else if (gradeList[i] > second && gradeList[i] !== max) { //nếu nó lớn hơn second và ko bằng max hiện tại thì sẽ gán nó = second
+            second = gradeList[i];
+        }
+    }
+    return [max, second];
 }
+console.log('1st grade: ' + findMaxAndSecond()[0]);
+console.log('2nd grade: ' + findMaxAndSecond()[1]);
+// console.log('1st grade: '+searchAverageFrist());
+// function searchAverageSecond() {
+//     let second=gradeList[0];
+//     let max=searchAverageFrist();
+//     for(let i=1; i<gradeList.length; i++){
+//         if(gradeList[i]>second && gradeList[i]<max){
+//             second = gradeList[i];
+//         }
+//     }
+//     return second;
+// }
+// console.log('2nd grade: '+searchAverageSecond());
+// sắp xếp danh sách tăng dần
+
+console.log(gradeList);
+// // interchange sort
+// function ArrageList() {
+//     for (let i = 0; i < gradeList.length; i++) {
+//         for (let j = i+1; j < gradeList.length; j++) {
+//             if (gradeList[i] > gradeList[j]) {
+//                 let temp = gradeList[i];
+//                 gradeList[i] = gradeList[j];
+//                 gradeList[j] = temp;
+//             }
+//         }
+//     }
+// }
+// // bubble sort
+function ArrageList() {
+    noSwap=true;
+    for (let i = gradeList.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if (gradeList[j] > gradeList[j + 1]) {
+                let temp = gradeList[j];
+                gradeList[j] = gradeList[j + 1];
+                gradeList[j + 1] = temp;
+                noSwap=false;
+            }
+            console.log('j:' + j, gradeList, gradeList[j], gradeList[j + 1]);
+        }
+        console.log('---------------------');
+        if(noSwap){
+            break;
+        }
+    }
+}
+console.log('interchange sort : ' + gradeList);
